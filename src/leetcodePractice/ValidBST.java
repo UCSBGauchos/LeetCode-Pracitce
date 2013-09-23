a8522719b18fd4d342274e118430cd5f59955c2b
yangboufo <130469Uf>
2013-09-22 22:04:10 -0700

    Valid BST

--- src/leetcodePractice/ValidBST.java
+++ src/leetcodePractice/ValidBST.java
@@ -38,7 +38,7 @@ public class ValidBST {
 		Node n1 = new Node(1);
 		Node n2 = new Node(2);
 		Node n3 = new Node(3);
-		Node n4 = new Node(6);
+		Node n4 = new Node(4);
 		Node n5 = new Node(5);
 		
 		n3.addLeft(n2);
