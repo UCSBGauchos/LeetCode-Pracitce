da35d60ab6ae76df5525a2ebb3039a8e4e62bc5e
yangboufo <130469Uf>
2013-09-25 23:50:30 -0700

    All subsets of a set

--- src/leetcodePractice/SubSet.java
+++ src/leetcodePractice/SubSet.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class SubSet {
 	
-	ArrayList<ArrayList<Integer>> allSubSets = new ArrayList<ArrayList<Integer>>();
+	//ArrayList<ArrayList<Integer>> allSubSets = new ArrayList<ArrayList<Integer>>();
 	public ArrayList<ArrayList<Integer>> allSubSet(List<Integer> set){
 		int last = set.get(0);
 		List<Integer> remain = set.subList(1, set.size());
@@ -17,18 +17,16 @@ public class SubSet {
 			list1.add(last);
 			lists.add(list1);
 			lists.add(list2);
-			allSubSets.add(list1);
-			allSubSets.add(list2);
 			return lists;
 		}
 		ArrayList<ArrayList<Integer>> preLists = allSubSet(remain);
 		ArrayList<ArrayList<Integer>> curLists = new ArrayList<ArrayList<Integer>>();
 		for(ArrayList<Integer> preList: preLists){
-			ArrayList<Integer> tmpList = new ArrayList<Integer>();
-			tmpList.addAll(preList);
-			tmpList.add(last);
-			curLists.add(tmpList);
-			allSubSets.add(tmpList);
+			ArrayList<Integer> newList = new ArrayList<Integer>();
+			curLists.add(preList);
+			newList.addAll(preList);
+			newList.add(last);
+			curLists.add(newList);
 		}
 		return curLists;
 		
@@ -57,8 +55,8 @@ public class SubSet {
 		
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		
-		ss.allSubSet(set);
-		
-		System.out.println(ss.removeDup(ss.allSubSets));
+		result = ss.allSubSet(set);
+	
+		System.out.println(ss.removeDup(result));
 	}
 }
