8567ceab3b42351cff8b9f22ea231177835f157b
UCSBGauchos <byang@cs.ucsb.edu>
2014-01-04 11:37:08 -0800

    Edit distance

--- src/leetcodePractice/EditDistance.java
+++ src/leetcodePractice/EditDistance.java
@@ -1,76 +1,58 @@
 package leetcodePractice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EditDistance {
 	
-	public ArrayList<String> insert(String word, char c){
-		ArrayList<String> newStrings = new ArrayList<String>();
-		for(int i=0; i<=word.length(); i++){
-			String firstPart = word.substring(0,i);
-			String SecondPart = word.substring(i);
-			String newString = firstPart+c+SecondPart;
-			newStrings.add(newString);
-		}
-		return newStrings;
-	}
 	
-	public ArrayList<String> replace(String word, ArrayList<Character> Dict){
-		ArrayList<String> newStrings = new ArrayList<String>();
-		for(char c: Dict){
-			for(int i=0; i<word.length(); i++){
-				if(!newStrings.contains(word.replace(word.charAt(i), c))){
-					newStrings.add(word.replace(word.charAt(i), c));
-				}
+	//this function will return all the possibilities 
+	public  HashMap<String, Boolean> helper(String word1, String word2) {
+		HashMap<String, Boolean> next = new  HashMap<String, Boolean>();
+		char [] dict = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		int result = 0;
+		
+		for(int i=0; i<=word1.length(); i++){
+			for(char c: dict){
+				StringBuffer buffer = new StringBuffer(word1);
+				buffer.insert(i, c);
+				String insertedString = buffer.toString();
+				next.put(insertedString, true);
 			}
-		}
-		return newStrings;
-	}
-	
-	public ArrayList<String> delete(String word){
-		ArrayList<String> newStrings = new ArrayList<String>();
-		for(int index = 0; index<word.length(); index++){
-			StringBuffer buffer = new StringBuffer(word);
-			buffer.deleteCharAt(index);
-			String newString = buffer.toString();
-			newStrings.add(newString);
-		}
-		return newStrings;
-	}
-	
-	ArrayList<String> has = new ArrayList<String>();
-	
-	public void minDistance(ArrayList<String> list, ArrayList<Character> Dict, String target){
-		ArrayList<String> levelList = new ArrayList<String>();
-		for(String str: list){
-			
-			ArrayList<String> oneStep = replace(str, Dict);
-//			for(String s: oneStep){
-//				System.out.println("The children of "+oneStep+" is "+s);
-//			}
-			for(String s: oneStep){
-				if(!has.contains(s)){
-					levelList.add(s);
-					has.add(s);
-				}
-			}
-		}
-	}
-	
-	
-	public static void main(String [] args){
-		String word1 = "ab";
-		String word2 = "ba";
-		ArrayList<Character> Dict = new ArrayList<Character>();
-		Dict.add('a');
-		Dict.add('b');
-		EditDistance ed = new EditDistance();
-		ArrayList<String> list = new ArrayList<String>();
-		list.add(word1);
-		ed.minDistance(list, Dict, word2);
-		for(String str: ed.has){
-			System.out.println(str);
 		}
 		
+		for(int i=0; i<word1.length(); i++){
+			StringBuffer buffer = new StringBuffer(word1);
+			buffer.deleteCharAt(i);
+			String removedString = buffer.toString();
+			next.put(removedString, true);
+		}
+		
+		for(int i=0; i<word1.length(); i++){
+			for(char c: dict){
+				StringBuffer buffer = new StringBuffer(word1);
+				buffer.setCharAt(i, c);
+				String modifiedString = buffer.toString();
+				next.put(modifiedString, true);
+			}
+		}
+		
+		
+		
+		
+			
+		return next;
+    }
+	
+	public static void main(String [] args){
+		String word1 = "abc";
+		String word2 = "abcde";
+		
+		EditDistance m  = new EditDistance();
+		HashMap<String, Boolean> result = m.helper(word1, word2);
+		for(String s: result.keySet()){
+			System.out.println(s);
+		}
 	}
+	
 }
