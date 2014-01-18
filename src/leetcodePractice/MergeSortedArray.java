751c6a9e51763163f4785048cb8fb7cec0e695fc
UCSBGauchos <byang@cs.ucsb.edu>
2014-01-18 15:47:42 -0800

    Merge Sorted Array

--- src/leetcodePractice/MergeSortedArray.java
+++ src/leetcodePractice/MergeSortedArray.java
@@ -1,47 +1,51 @@
 package leetcodePractice;
 
 public class MergeSortedArray {
-//	
-	public int [] merge(int [] a, int [] b){
-		int [] helper = new int [a.length];
-		int indexForA = (a.length-1)/2;
-		int indexForB = b.length-1;
-		int indexForHelper = helper.length-1;
-		while(indexForHelper>=0){
-			if(indexForA>=0&&indexForB>=0&&a[indexForA]>b[indexForB]){
-				helper[indexForHelper] = a[indexForA];
-				indexForHelper--;
-				indexForA--;
-			}else if(indexForA>=0&&indexForB>=0&&b[indexForB]>a[indexForA]){
-				helper[indexForHelper] = b[indexForB];
-				indexForHelper--;
-				indexForB--;
-			}else if(indexForB<0){
-				helper[indexForHelper] = a[indexForA];
-				indexForHelper--;
-			}else if(indexForA<0){
-				helper[indexForHelper] = b[indexForB];
-				indexForHelper--;
+	
+	public void merge(int A[], int m, int B[], int n) {
+		int index = A.length-1;
+		int indexA = m-1;
+		int indexB = n-1;
+		
+		while(index>=0){
+			if(indexA>=0&&indexB>=0&&A[indexA]>=B[indexB]){
+				A[index] = A[indexA];
+				index--;
+				indexA--;
+			}else if(indexA>=0&&indexB>=0&&A[indexA]<B[indexB]){
+				A[index] = B[indexB];
+				index--;
+				indexB--;
+			}else if(indexA < 0){
+				A[index] = B[indexB];
+				index--;
+				indexB--;
+			}else if(indexB<0){
+				A[index] = A[indexA];
+				index--;
+				indexA--;
 			}
 		}
-		return helper;
-		
-	}
+    }
+	
+	
 	
 	public static void main(String [] args){
-		int [] a = new int [10];
-		int [] b = new int [5];
-		for(int i = 0; i< a.length/2; i++){
+		int m = 5;
+		int n = 5;
+		
+		int [] a = new int [m+n];
+		int [] b = new int [n];
+		for(int i = 0; i< m; i++){
 			a[i] = 2*i+1;
 		}
-		for(int i = 0; i< b.length; i++){
+		for(int i = 0; i< n; i++){
 			b[i] = 2*i+2;
 		}
-		MergeSortedArray msa = new MergeSortedArray();
-		int [] sortedArray = msa.merge(a, b);
-		for(int i: sortedArray){
-			System.out.println(i);
+		MergeSortedArray merge = new MergeSortedArray();
+		merge.merge(a, m, b, n);
+		for(int i: a){
+			System.out.print(i);
 		}
-		
 	}
 }
