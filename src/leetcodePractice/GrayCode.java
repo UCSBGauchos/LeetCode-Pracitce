bfa9611cb864591bf4ca48f55e705d55e2c616e8
UCSBGauchos <byang@cs.ucsb.edu>
2014-01-19 01:45:31 -0800

    Gray Code
    Just one instance

--- src/leetcodePractice/GrayCode.java
+++ src/leetcodePractice/GrayCode.java
@@ -1,50 +1,64 @@
 package leetcodePractice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GrayCode {
 	
-	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-	public void createGrayCode(ArrayList<Integer> list, int index, int number){
-		if(index == number){
-			return;
+	public int stringToInt(String str){
+		int number = 0;
+		int length = str.length();
+		for(int i=0; i<length; i++){
+			number+=(str.charAt(i)-48)*Math.pow(2, length-1-i);
 		}
-		for(int i=0;i<list.size(); i++){
-			if(!result.contains(list)){
-				ArrayList<Integer> newList = new ArrayList<Integer>();
-				newList.addAll(list);
-				result.add(newList);
-				index++;
-			}
-			if(list.get(i)==0){
-				list.set(i, 1);
-				if(!result.contains(list)){
-					createGrayCode(list, index, number);
-				}else{
-					list.set(i, 0);
+		return number;
+	}
+	
+	public void help(String str, HashMap<String, Boolean> cache, ArrayList<Integer> list){
+		int number = stringToInt(str);
+		if(!list.contains(number)){
+			list.add(number);
+		}
+		for(int i=0; i<str.length(); i++){
+			if(str.charAt(i)=='0'){
+				StringBuffer buffer = new StringBuffer(str);
+				buffer.setCharAt(i, '1');
+				String newString = buffer.toString();
+				if(!cache.containsKey(newString)){
+					cache.put(newString, true);
+					help(newString, cache, list);
 				}
-			}
-			if(list.get(i)==1){
-				list.set(i, 0);
-				if(!result.contains(list)){
-					createGrayCode(list, index, number);
-				}else{
-					list.set(i, 1);
+			}else if(str.charAt(i)=='1'){
+				StringBuffer buffer = new StringBuffer(str);
+				buffer.setCharAt(i, '0');
+				String newString = buffer.toString();
+				if(!cache.containsKey(newString)){
+					cache.put(newString, true);
+					help(newString, cache, list);
 				}
 			}
 		}
 	}
 	
+	public ArrayList<Integer> grayCode(int n) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		StringBuffer buffer = new StringBuffer();
+		for(int i=0; i<n; i++){
+			buffer.append(0);
+		}
+		HashMap<String, Boolean> cache = new HashMap<String, Boolean>();
+		help(buffer.toString(), cache, result);
+		return result;
+    }
+	
 	
 	public static void main(String [] args){
-		int number = 2;
-		int allGrayNumber = (int)Math.pow(2, number);
-		ArrayList<Integer> Begin = new ArrayList<Integer>();
-		for(int index = 0; index< number; index++){
-			Begin.add(0);
+		GrayCode g = new GrayCode();
+		int n = 2;
+		ArrayList<Integer> list = g.grayCode(n);
+		for(int i: list){
+			System.out.println(i);
 		}
-		GrayCode gc = new GrayCode();
-		gc.createGrayCode(Begin, 0, allGrayNumber);
-		System.out.println(gc.result);
+		
 	}
 }
