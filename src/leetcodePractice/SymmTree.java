3ac425d9fdb1178881d627fe7c5525d03d8018b2
UCSBGauchos <byang@cs.ucsb.edu>
2014-01-23 23:30:26 -0800

    Symm Tree

--- src/leetcodePractice/SymmTree.java
+++ src/leetcodePractice/SymmTree.java
@@ -5,88 +5,86 @@ import java.util.Stack;
 
 public class SymmTree {
 	
-	ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
-	public void createlists(Node node, int level){
-		if(node == null){
+	public void help(TreeNode root, int level, ArrayList<ArrayList<String>> result){
+		if(root == null){
+			if(level == result.size()){
+				ArrayList<String> list = new ArrayList<String>();
+				list.add("#");
+				result.add(list);
+			}else{
+				result.get(level).add("#");
+			}
 			return;
 		}
-		if(level == lists.size()){
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			list.add(node.value);
-			lists.add(list);
+		if(level == result.size()){
+			ArrayList<String> list = new ArrayList<String>();
+			list.add(String.valueOf(root.val));
+			result.add(list);
 		}else{
-			ArrayList<Integer> list = lists.get(level);
-			list.add(node.value);
+			result.get(level).add(String.valueOf(root.val));
 		}
-		createlists(node.left,level+1);
-		createlists(node.right,level+1);
+		help(root.left, level+1, result);
+		help(root.right, level+1, result);
 	}
 	
-	public boolean determinPali(ArrayList<Integer> list){
-		int midIndex = (list.size()-1)/2;
+	public ArrayList<ArrayList<String>> levelOrder(TreeNode root) {
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		int level = 0;
+		help(root, level, result);
+		return result;
+    }
+	
+	public boolean isPali(ArrayList<String> list){
 		int length = list.size();
-		if(length%2==0){
-			Stack<Integer> stack = new Stack<Integer>();
-			int i=0;
-			for(i=0;i<=midIndex;i++){
-				stack.push(list.get(i));
-			}
-			for(int j=i;j<length;j++){
-				if(stack.pop()!=list.get(j)){
+		Stack<String> stack = new Stack<String>();
+		int i=0;
+		int j=0;
+		for(i=0; i<=length/2-1; i++){
+			stack.add(list.get(i));
+		}
+		if(length%2 == 0){
+			for(j = i; j<length; j++){
+				if(!stack.pop().equals(list.get(j))){
 					return false;
 				}
 			}
-		}else if(length%2!=0){
-			Stack<Integer> stack = new Stack<Integer>();
-			int i=0;
-			for(i=0;i<midIndex;i++){
-				stack.push(list.get(i));
-			}
-			i++;
-			for(int j=i;j<length;j++){
-				if(stack.pop()!=list.get(j)){
+		}else{
+			for(j=i+1; j<length; j++){
+				if(stack.pop().equals(list.get(j))){
 					return false;
 				}
 			}
 		}
 		return true;
-		
 	}
 	
-	public boolean determinSymm(ArrayList<ArrayList<Integer>> lists){
-		for(int index = 0; index<lists.size(); index++){
-			if(index != 0 && lists.get(index).size()%2!=0){
-				return false;
-			}
-			if(!determinPali(lists.get(index))){
+	public boolean isSymmetric(TreeNode root) {
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		result = levelOrder(root);
+		for(ArrayList<String> list: result){
+			if(!isPali(list)){
 				return false;
 			}
 		}
 		return true;
 	}
-	
 	
 	public static void main(String [] args){
-		Node level1N1 = new Node(1);
-		Node level2N1 = new Node(2);
-		Node level2N2 = new Node(2);
-		Node level3N1 = new Node(3);
-		Node level3N2 = new Node(4);
-		Node level3N3 = new Node(4);
-		Node level3N4 = new Node(3);
-		
-		level1N1.addLeft(level2N1);
-		level1N1.addRight(level2N2);
-		level2N1.addLeft(level3N1);
-		level2N1.addRight(level3N2);
-		level2N2.addLeft(level3N3);
-		level2N2.addRight(level3N4);
-		
-		ArrayList<Integer> testList = new ArrayList<Integer>();
-		
-		SymmTree st = new SymmTree();
-		st.createlists(level1N1, 0);
-		System.out.println(st.determinSymm(st.lists));
-		
+		SymmTree s = new SymmTree();
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(2);
+		TreeNode n4 = new TreeNode(3);
+		TreeNode n5 = new TreeNode(4);
+		TreeNode n6 = new TreeNode(4);
+		TreeNode n7 = new TreeNode(3);
+		n1.left = n2;
+		n1.right = n3;
+		n2.left = n4;
+		n2.right = n5;
+		n3.left = n6;
+		n3.right = n7;
+		System.out.println(s.isSymmetric(n1));
 	}
+	
 }
