1e3da27ac0711a6efa487662458b800f0d313a0e
UCSBGauchos <byang@cs.ucsb.edu>
2014-01-26 00:05:31 -0800

    The answer is correct. But the output structure is not good

--- src/leetcodePractice/PalindromePartition.java
+++ src/leetcodePractice/PalindromePartition.java
@@ -4,127 +4,63 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class PalindromePartition {
-	   public boolean isOven(int length){
-			if(length%2 == 0){
-				return true;
-			}else{
-				return false;
-			}
+	
+	public boolean isPali(String s){
+		char [] charArray = s.toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		int length = charArray.length;
+		int i = 0;
+		int j=0;
+		for(i=0; i<length/2; i++){
+			stack.add(charArray[i]);
 		}
-		public boolean isPali(String str){
-			if(str == ""){
-				return true;
-			}
-			int mid = str.length()/2;
-			int after = 0;
-			char [] charArray = str.toCharArray();
-			Stack<Character> stack = new Stack<Character>();
-			for(int index = 0; index < mid; index++){
-				stack.push(charArray[index]);
-			}
-			if(isOven(str.length())){
-				after = mid;
-			}else{
-				after = mid+1;
-			}
-			for(int index = after; index<str.length(); index++){
-				if(stack.pop()!=charArray[index]){
+		if(length%2 == 0){
+			for(j=i; j<charArray.length; j++){
+				if(stack.pop()!=charArray[j]){
 					return false;
 				}
 			}
-			return true;
-		}
-		
-		public void getSubString(String str){
-			for(int i=1;i<str.length();i++){
-				String front = str.substring(0,i);
-				String behind = str.substring(i);
-				//System.out.println(front);
-				//System.out.println(behind);
-			}
-		}
-		
-		public void minCutMainFucntion(String str, int number, int [] mins, int level, ArrayList<Integer> tmp, int index, int length){
-			//System.out.println(str);
-			if(isPali(str)){
-				tmp.add(0);
-				return;
-			}
-			for(int i=1;i<str.length();i++){
-				if(level == 1){
-					index++;
-					number = 0;
-				}
-				String front = str.substring(0,i);
-				String behind = str.substring(i);
-				int currentNum = number;
-				currentNum ++;
-				if(isPali(front)&&isPali(behind)){
-					//System.out.println("they are "+front+" "+behind);
-					//System.out.println("after sub "+(length - front.length()-behind.length())+" left");
-					mins[index] = currentNum;
-					if(length - front.length()-behind.length()==0){
-						//tmp.push(number);
-						//System.out.println("length get "+(length - front.length()-behind.length())+" so end");
-						//System.out.println("find one cut is "+currentNum);
-						tmp.add(currentNum);
-						//int newLength = length - front.length()-behind.length();
-					}
-				}else if(!isPali(front)&&isPali(behind)){
-					int newLevel = level+1;
-					int newLength = length - behind.length();
-					//System.out.println("after sub "+newLength+" left");
-					//System.out.println("processing "+front);
-					minCutMainFucntion(front, currentNum, mins, newLevel, tmp, index, newLength);
-				}
-				else if(!isPali(behind)&&isPali(front)){
-					int newLevel = level+1;
-					int newLength = length - front.length();
-					//System.out.println("after sub "+newLength+" left");
-					//System.out.println("processing "+behind);
-					minCutMainFucntion(behind, currentNum, mins, newLevel, tmp, index, newLength);
-				}
-				else if(!isPali(behind)&&!isPali(front)){
-					//System.out.println("processing "+front+behind);
-					int newLevel = level+1;
-					//System.out.println(length+" left");
-					int newLength = length;
-					minCutMainFucntion(front, currentNum, mins, newLevel, tmp, index, newLength);
-					currentNum = mins[index];
-					newLength = newLength-front.length();
-					minCutMainFucntion(behind, currentNum, mins, newLevel, tmp, index, newLength);
+		}else{
+			for(j=i+1; j<charArray.length; j++){
+				if(stack.pop()!=charArray[j]){
+					return false;
 				}
 			}
 		}
-		
-		public int getMinValue(ArrayList<Integer> array){
-			int min = Integer.MAX_VALUE;
-			for(int i=0;i<array.size();i++){
-				if(array.get(i)<min){
-					min = array.get(i);
-				}
+		return true;
+	}
+	
+	public void help(String s, ArrayList<ArrayList<String>> result, ArrayList<String> list){
+		if(isPali(s)){
+			list.add(s);
+			ArrayList<String> SnapShot = new ArrayList<String>();
+			for(String str: list){
+				SnapShot.add(str);
 			}
-			return min;
+			result.add(SnapShot);
+			list.remove(s);
 		}
-			
-		public int minCut(String str){
-			int level = 1;
-			int [] mins = new int[100];
-			for(int i = 0; i< mins.length; i++){
-				mins[i] = Integer.MAX_VALUE;
+		for(int i=1; i<s.length(); i++){
+			String last = s.substring(0,i);
+			if(isPali(last)){
+				list.add(last);
+				String remain =s.substring(i);
+				help(remain, result, list);
+				list.remove(last);
 			}
-			for(int index = 0; index<str.length(); index++){
-				mins[index] = Integer.MAX_VALUE;
-			}
-			ArrayList<Integer> tmp = new ArrayList<Integer>();
-			minCutMainFucntion(str, 0, mins, level, tmp, 0, str.length());
-			return getMinValue(tmp);
 		}
-		
-		public static void main(String [] args){
-			PalindromePartition pali = new PalindromePartition();
-			String str = "a";//c aba b cbc
-		
-			System.out.println(	pali.minCut(str));
-		}
+	}
+	
+	public ArrayList<ArrayList<String>> partition(String s) {
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		ArrayList<String> list = new ArrayList<String>();
+		help(s, result, list);
+		return result;
+    }
+	
+	public static void main(String [] args){
+		PalindromePartition p = new PalindromePartition();
+		String s = "cbbbcc";
+		System.out.println(p.partition(s));
+	}
 }
